3c195345ddc17c4a772503f6ec6389cb18d00e47
FFenixx <paraopedro101@gmail.com>
2021-02-06 13:58:05 -0300

    fix bug

--- utils/ArgsUtil.js
+++ utils/ArgsUtil.js
@@ -11,16 +11,17 @@ const command = args.shift().toLowerCase();
 const cmd = message.client.commands.get(command) || message.client.commands.get(message.client.aliases.get(command));
 if(!cmd) return;
 
-var victims = args.join(" ").replace(TIME_REGEX, '').split(/\s+/g).filter(String).concat("").map(a => a.replace(/[^0-9]+/g, ""))
+var victims = args.concat("").map(a => a.replace(/[^0-9]+/g, ""));
 victims = victims.slice(0, victims.indexOf(""));
-
 var reason = args.slice(victims.length, args.length), time;
 
 const isDuration = (str) => /([0-9]+) ?(year|years|y)|([0-9]+) ?(week|weeks|w)|([0-9]+) ?(day|days|d)|([0-9]+) ?(hours|hour|h)|([0-9]+) ?(minutes|minute|mins|min|m)|((?:[0-9]*[.])?[0-9]+) ?(seconds|second|secs|sec|s)/i.exec(str);
 if(allowTime)
 {
 var regTime;
-
+victims = args.join(" ").replace(TIME_REGEX, '').split(/\s+/g).filter(String).concat("").map(a => a.replace(/[^0-9]+/g, ""));
+victims = victims.slice(0, victims.indexOf(""));
+reason = args.slice(victims.length, args.length);
 if(victims[0] && reason[0] && !reason.join(" ").trim().replace(TIME_REGEX, "").split(/\s+/g).filter(String)[0] && isDuration(reason.join(" "))) regTime = false;
     if(victims[0] && reason[0] && regTime != false && isDuration(reason.slice(0, reason.indexOf(reason.join(" ").trim().replace(TIME_REGEX, '').split(/\s+/g).filter(String)[0])).join(" ")) && isDuration(reason.slice(0, reason.indexOf(reason.join(" ").trim().replace(TIME_REGEX, '').split(/\s+/g).filter(String)[0])).join(" ")).index == 0) {
       time = reason.slice(0, reason.indexOf(reason.join(" ").trim().replace(TIME_REGEX, '').split(/\s+/g).filter(String)[0])).join(" ")
