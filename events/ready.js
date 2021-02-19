d27b9a52bbb6903fd7b4abc75759e490cf75fee3
Gangexe <paraopedro101@gmail.com>
2021-02-19 13:14:56 +0000

    .

--- events/ready.js
+++ events/ready.js
@@ -11,9 +11,9 @@ module.exports = class {
 
   async run () {
     this.client.user.setActivity(`Use ${this.client.config.prefix}help`);
-    setInterval(() => {
-      postStats(this.client);
-    }, 3e5);
+    //setInterval(() => {
+      //postStats(this.client);
+    //}, 3e5);
     setInterval(async() => {
       await PremiumManager.checkExpirations(this.client);
       TempMuteManager.checkUnmutes(this.client);
