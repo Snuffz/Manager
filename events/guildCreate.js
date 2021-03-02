7fa57d6522e867ead79ba32628830cf0d926fe9c
FFenixx <paraopedro101@gmail.com>
2021-03-02 20:19:48 -0300

    fixing bug

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -15,13 +15,16 @@ module.exports = class {
   async run (guild) {
     if(!guild.name)
     return;
-      const settings = await Settings.findOne({ guildID: guild.id });
-   if (!settings) 
-   return;
+    setTimeout(() => { 
+      if(guild.members.cache.filter(a => !a.user.bot).size < 15) {
+        guild.owner.send("Your server does not have the basic requirements to have me, it has very few members. The minimum is 15.").catch(()=>{});
+        guild.leave();
+      }
+    }, 300000);
 const tc = this.client.channels.cache.get(this.client.config.logsChannel);
 if(!tc)
 return;
-const date = moment(Date.now()).tz(settings.timezone).format("hh:mm:ss");
+const date = moment(Date.now()).tz("America/New_York").format("hh:mm:ss");
     const eb = new MessageEmbed()
       .setThumbnail(guild.iconURL({ format: 'png', dynamic: true, size: 1024 }))
       .setDescription(`
@@ -35,11 +38,5 @@ ${LINESTART} Number of Servers Currently: **${this.client.guilds.cache.size}**
       .setColor(this.client.config.color)
     tc.send({content:`\`[${date}]\`
 ${NEW} I logged into the server **${guild.name}**:`,embed: eb, disableMentions: "all" });
-    setTimeout(() => { 
-      if(guild.members.cache.filter(a => !a.user.bot).size < 15) {
-        guild.owner.send("Your server does not have the basic requirements to have me, it has very few members. The minimum is 15.").catch(()=>{});
-        guild.leave();
-      }
-    }, 300000);
   }
 }
