e379a6c1f28dd93980244fb4524484b94039e1ee
FFenixx <paraopedro101@gmail.com>
2021-03-05 14:45:56 -0300

    a

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,6 @@
 const 
 Settings = require("../models/settings.js"),
+Constants = require("../config.js"),
 { MessageEmbed } = require("discord.js"),
 FormatUtil = require("../utils/FormatUtil"),
 moment = require("moment-timezone");
@@ -15,12 +16,7 @@ module.exports = class {
   async run (guild) {
     if(!guild.name)
     return;
-    setTimeout(() => { 
-      if(guild.members.cache.filter(a => !a.user.bot).size < 15) {
-        guild.owner.send("Your server does not have the basic requirements to have me, it has very few members. The minimum is 15.").catch(()=>{});
-        guild.leave();
-      }
-    }, 300000);
+    setTimeout(async () => { await leavePointlessGuilds() }, 300000);
 const tc = this.client.channels.cache.get(this.client.config.logsChannel);
 if(!tc)
 return;
@@ -39,4 +35,34 @@ ${LINESTART} Number of Servers Currently: **${this.client.guilds.cache.size}**
     tc.send({content:`\`[${date}]\`
 ${NEW} I logged into the server **${guild.name}**:`,embed: eb, disableMentions: "all" });
   }
+}
+
+function leavePointlessGuilds()
+{
+  this.client.guilds.cache.filter(async g => 
+    {
+      if(!g.ownerID)
+        return false;
+      if(Constants.OWNER_ID==g.ownerID)
+        return false;
+      const settings = await Settings.findOne({ guildID: g.id });
+      const botcount = g.members.cache.filter(m => m.user.bot).size;
+      const totalcount = g.members.cache.size;
+      const humancount = totalcount - botcount;
+      if(humancount < 15 || botcount > humancount)
+      {
+     if(settings)
+       return false;
+       return true;
+      }
+    return false;
+    }).forEach(g => 
+      {
+        if(g.owner)
+        {
+          g.owner.send(`${this.client.config.emojis.error} Your server **${g.name}** does not have the basic requirements to have me, it has very few members. The minimum is 15.`)
+          .then(() => g.leave())
+          .catch(() => g.leave());
+        }
+      })
 }
