27058cdd21cdb7543d1793af9b6c3c7192b77aa7
Snuffz <51891016+Snuffz@users.noreply.github.com>
2024-01-07 02:09:44 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 const config = {
-  "token":  "NzMyMjM3NjAwNjA1MjA4NjI2.XwxraA.XGuwzBb23nTFRch6eo1CemixYV4",
+  "token":  "",
   "prefix": "..",
-  "dbUrl": "mongodb+srv://snuff:e270906@modbot.rcmli.azure.mongodb.net/modbot?retryWrites=true&w=majority",
-  // mongodb+srv://Snuff:e270906@modmail.tzmhg.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+  "dbUrl": "mongodb+srv://snuff:@modbot.rcmli.azure.mongodb.net/modbot?retryWrites=true&w=majority",
   "SERVER_INVITE": 'https://discord.gg/eEQBT4A',
   "OWNER_ID": '602140733985652759',
   "donation_link": 'https://patreon.com/snuff',
   "color": "7289da",
   "bot_invite": "https://discordapp.com/oauth2/authorize?client_id=732237600605208626&permissions=500559086&scope=bot",
-  "iblToken": "y1DxKpraM16TVbNfyeK1WSEN7mnk9bhTPMuJ31vNbTP9lqJKbY7sFUCFaQ7l5CWxATkJENvY9jpi9Zz7FYCKfQUzeuV8GCB8LuG5",
+  "iblToken": "",
 
   "emojis": {
  "success": "<:check:726180776294744275>",
@@ -29,6 +28,6 @@ const config = {
     "STRIKES_GUIDE": "https://github.com/Snuffz/ManagerBot/wiki/Strikes",
     "RAID_MODE": "https://github.com/Snuffz/ManagerBot/wiki/Anti-Raid"
   },
-  "logsChannel": "801960202416554024",
+  "logsChannel": "80196020241655402",
 };
 module.exports = config;
